993bfb1d83d6d2daf273cced80ca69af47d0f6ec
Roman Kaluger <r.kaluger@sftpro.ru>
2022-06-05 23:36:32 +0300

    try

--- src/app.module.ts
+++ src/app.module.ts
@@ -12,17 +12,24 @@ import { WordsModule } from './wordsToLearn/wordsToLearn.module';
     ConfigModule.forRoot({ isGlobal: true }),
     SequelizeModule.forRootAsync({
       inject: [ConfigService],
-      useFactory: (config: ConfigService) => ({
-        dialect: config.get<Dialect>(DB_VARIABLES.DATABASE_DIALECT),
-        host: config.get<string>(DB_VARIABLES.DATABASE_HOST),
-        port: config.get<number>(DB_VARIABLES.DATABASE_PORT),
-        username: config.get<string>(DB_VARIABLES.DATABASE_USER),
-        password: config.get<string>(DB_VARIABLES.DATABASE_PASSWORD),
-        database: config.get<string>(DB_VARIABLES.DATABASE_NAME),
-        autoLoadModels: true,
-        synchronize: true,
-        logging: false,
-      }),
+      useFactory: (config: ConfigService) =>
+        config.get<string>(DB_VARIABLES.DATABASE_URL)
+          ? {
+              host: config.get<string>(DB_VARIABLES.DATABASE_URL),
+              autoLoadModels: true,
+              synchronize: true,
+            }
+          : {
+              dialect: config.get<Dialect>(DB_VARIABLES.DATABASE_DIALECT),
+              host: config.get<string>(DB_VARIABLES.DATABASE_HOST),
+              port: config.get<number>(DB_VARIABLES.DATABASE_PORT),
+              username: config.get<string>(DB_VARIABLES.DATABASE_USER),
+              password: config.get<string>(DB_VARIABLES.DATABASE_PASSWORD),
+              database: config.get<string>(DB_VARIABLES.DATABASE_NAME),
+              autoLoadModels: true,
+              synchronize: true,
+              logging: false,
+            },
     }),
     AuthModule,
     WordsModule,
