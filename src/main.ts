bf91669ba4ac2f17fac8dcbac953228fc89f5acc
Roman Kaluger <r.kaluger@sftpro.ru>
2022-06-05 20:49:00 +0300

    add static

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,19 @@
 import { NestFactory, Reflector } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { join } from 'path';
 import { AppModule } from './app.module';
 import { JwtAuthGuard } from './auth/jwt-auth.guard';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, { cors: true });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
   const reflector = app.get(Reflector);
 
   app.useGlobalGuards(new JwtAuthGuard(reflector));
   app.enableShutdownHooks();
+
+  app.useStaticAssets(join(__dirname, '..', '..', 'client', 'build'));
 
   await app.listen(3000);
 }
