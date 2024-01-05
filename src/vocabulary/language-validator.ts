b6fa6c205dfbd60c219be8e6658d54d9cd8523c4
Roman Kaluger <r.kaluger@sftpro.ru>
2024-01-05 23:58:30 +0600

    validator lang

--- src/vocabulary/language-validator.ts
+++ src/vocabulary/language-validator.ts
@@ -2,8 +2,8 @@ import { Lang } from "../types";
 
 class LanguageValidator {
   private languageRegexps: Record<Lang, RegExp> = {
-    [Lang.ru]: /[а-я, ]+/gi,
-    [Lang.en]: /[\w, ]+/gi,
+    [Lang.ru]: /^[а-я, ]+$/gi,
+    [Lang.en]: /^[\w, ]+$/gi,
   };
 
   validate(text: string, lang: Lang) {
